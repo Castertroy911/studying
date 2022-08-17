0eb5276e1cf94428d0f51dc52c2b4829c5a7c6df
Dima Leonov <d.leonov@m2e.team>
2022-08-17 13:01:40 +0300

    Normal work of "assert"

--- test_items.py
+++ test_items.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 class TestItems:
     def test_add_to_card_button(self, browser):
-        browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-        button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-        assert button, "Add to basket button isn't here :)"
+        try:
+            browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+            button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket1")
+        except NoSuchElementException as err:
+            assert err is None, "Add to basket button isn't here :)"
